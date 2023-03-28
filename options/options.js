2e0440900ceda5dfedd1d8f288ab87e693f0f0fa
Saurav <sp.gharti@gmail.com>
2023-03-28 14:15:09 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "noisy-hill-2",
+  url: "https://noisy-hill-2.botics.co",
+  api: "https://noisy-hill-2.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
